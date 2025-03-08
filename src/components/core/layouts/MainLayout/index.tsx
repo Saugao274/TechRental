660abc870d473cd40ec1fd030b4ac0a9c6f6c99c
Saugao274 <thuynttde170035@fpt.edu.vn>
2025-03-08 19:04:19 +0700

    update

--- src/components/core/layouts/MainLayout/index.tsx
+++ src/components/core/layouts/MainLayout/index.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { Layout } from 'antd'
+import { Breadcrumb, Layout, Menu } from 'antd'
 import { Content, Footer, Header } from 'antd/es/layout/layout'
 
 interface MainLayoutProps {
@@ -10,9 +10,34 @@ interface MainLayoutProps {
 function MainLayout({ children }: MainLayoutProps) {
     return (
         <Layout>
-            <Header />
-            <Content>{children}</Content>
-            <Footer />
+            <Header style={{ position: 'fixed', width: '100%' }}>
+                <div className="logo" />
+                <Menu
+                    theme="dark"
+                    mode="horizontal"
+                    defaultSelectedKeys={['2']}
+                    style={{ lineHeight: '64px' }}
+                >
+                    <Menu.Item key="1">nav 1</Menu.Item>
+                    <Menu.Item key="2">nav 2</Menu.Item>
+                    <Menu.Item key="3">nav 3</Menu.Item>
+                </Menu>
+            </Header>
+            <Content style={{ padding: '0 50px', marginTop: 64 }}>
+                <Breadcrumb style={{ margin: '16px 0' }}>
+                    <Breadcrumb.Item>Home</Breadcrumb.Item>
+                    <Breadcrumb.Item>List</Breadcrumb.Item>
+                    <Breadcrumb.Item>App</Breadcrumb.Item>
+                </Breadcrumb>
+                <div
+                    style={{ background: '#fff', padding: 24, minHeight: 380 }}
+                >
+                    {children}
+                </div>
+            </Content>
+            <Footer style={{ textAlign: 'center' }}>
+                Ant Design ©2016 Created by Ant UED
+            </Footer>
         </Layout>
     )
 }
