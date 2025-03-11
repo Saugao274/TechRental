04f7cc5e07a20b5cf7f4063f260634b7c1922d89
From_VuVuDeveloper <vuvo070403@gmail.com>
2025-03-11 21:34:12 +0700

    [VuVu] - Update header

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,22 +1,8 @@
 import MainLayout from '@/components/core/layouts/MainLayout'
-import { cn } from '@/libs/utils'
 export default async function LayoutAuth({
     children,
 }: {
     readonly children: React.ReactNode
 }) {
-    return (
-        <div className="relative">
-            <div
-                className={cn(
-                    'z-[-1000] h-screen w-full overflow-y-auto',
-                    // "bg-[url('https://img.freepik.com/premium-vector/abstract-realistic-blue-sky-with-clouds-background_38782-305.jpg?semt=ais_hybrid')] bg-cover bg-no-repeat blur-3xl",
-                    "bg-white"
-                )}
-            ></div>
-            <div className="absolute top-0 z-[1] h-screen w-full overflow-hidden overflow-y-auto overflow-x-hidden">
-                <MainLayout>{children}</MainLayout>
-            </div>
-        </div>
-    )
+    return <MainLayout>{children}</MainLayout>
 }
