4d30a82ad994fde4f01fc95242431ab0ab4c41a3
Saugao274 <thuynttde170035@fpt.edu.vn>
2025-03-11 15:04:27 +0700

    [ThuyNTT] Slider

--- src/components/modules/Home/index.tsx
+++ src/components/modules/Home/index.tsx
@@ -3,12 +3,13 @@ import SectionCommon from '@/components/core/common/SectionCommon'
 import News from '@/components/core/elements/News'
 import HotProducts from '@/components/core/elements/Products/HotProducts'
 import NewProduct from '@/components/core/elements/Products/NewProduct'
+import Slider from '@/components/core/elements/Slider'
 import React from 'react'
 
 const Home = () => {
     return (
-        <div className='max-w-[1440px] mx-auto'>
-            <SectionCommon>HOME</SectionCommon>
+        <div className="mx-auto max-w-[1440px]">
+            <Slider />
             <HotProducts />
             <NewProduct />
             <News />
