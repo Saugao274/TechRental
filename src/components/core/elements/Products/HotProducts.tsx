570d6184a6dc4555afbe84e5a6e6b2ec5bb4888b
Saugao274 <ntthanhthuy274@gmail.com>
2025-05-16 17:49:25 +0700

    [ThuyNTT] Loading

--- src/components/core/elements/Products/HotProducts.tsx
+++ src/components/core/elements/Products/HotProducts.tsx
@@ -4,11 +4,16 @@ import ProductCard from '../../common/CardCommon/ProductCard'
 import SectionCommon from '../../common/SectionCommon'
 import { getRequest } from '@/request'
 import { productEndpoint } from '@/settings/endpoints'
+import { useAuth } from '@/context/AuthContext'
+import { Skeleton } from 'antd'
 
 const HotProducts = () => {
     const [productsData, setProductsData] = useState<any[]>([])
+    const [loading, setLoading] = useState(false)
+
     useEffect(() => {
         const fetchProducts = async () => {
+            setLoading(true)
             try {
                 const responseAllProduct = await getRequest(
                     productEndpoint.GET_ALL,
@@ -17,21 +22,44 @@ const HotProducts = () => {
             } catch (error) {
                 console.error('Error fetching products:', error)
             }
+            setLoading(false)
         }
 
         fetchProducts()
-    }, [productsData])
+    }, [])
     const hotProductssData = productsData.filter(
         (product) => product.isHotProduct,
     )
     return (
         <SectionCommon className="flex flex-col gap-10">
             <PageHader title="Sản phẩm nổi bật" />
-            <div className="grid grid-cols-1 gap-5 md:grid-cols-4">
-                {hotProductssData.slice(0, 4).map((product, index) => (
-                    <ProductCard product={product} key={index} />
-                ))}
-            </div>
+            {!loading ? (
+                <div className="grid grid-cols-1 gap-5 md:grid-cols-4">
+                    {hotProductssData.slice(0, 4).map((product, index) => (
+                        <ProductCard product={product} key={index} />
+                    ))}
+                </div>
+            ) : (
+                <Skeleton
+                    active
+                    paragraph={{ rows: 4 }}
+                    className="h-full w-full"
+                >
+                    <SectionCommon className="flex flex-col gap-10">
+                        <PageHader title="Sản phẩm nổi bật" />
+                        <div className="grid grid-cols-1 gap-5 md:grid-cols-4">
+                            {hotProductssData
+                                .slice(0, 4)
+                                .map((product, index) => (
+                                    <ProductCard
+                                        product={product}
+                                        key={index}
+                                    />
+                                ))}
+                        </div>
+                    </SectionCommon>
+                </Skeleton>
+            )}
         </SectionCommon>
     )
 }
--- src/components/core/elements/Products/NewProduct.tsx
+++ src/components/core/elements/Products/NewProduct.tsx
@@ -4,23 +4,28 @@ import ProductCard from '../../common/CardCommon/ProductCard'
 import SectionCommon from '../../common/SectionCommon'
 import { productEndpoint } from '@/settings/endpoints'
 import { getRequest } from '@/request'
+import { useAuth } from '@/context/AuthContext'
+import { Skeleton } from 'antd'
 
 const NewProduct = () => {
     const [productsData, setProductsData] = useState<any[]>([])
+    const [loading, setLoading] = useState(false)
     useEffect(() => {
         const fetchProducts = async () => {
+            setLoading(true)
             try {
                 const responseAllProduct = await getRequest(
                     productEndpoint.GET_ALL,
                 )
                 setProductsData(responseAllProduct.metadata)
+                setLoading(false)
             } catch (error) {
                 console.error('Error fetching products:', error)
             }
         }
 
         fetchProducts()
-    }, [productsData])
+    }, [])
     const newProductsData = productsData.filter(
         (product) => product.isNewProduct,
     )
@@ -29,11 +34,33 @@ const NewProduct = () => {
             <div>
                 <PageHader title="Sản phẩm mới" />
             </div>
-            <div className="grid grid-cols-1 gap-5 md:grid-cols-4">
-                {newProductsData.slice(0, 4).map((product, index) => (
-                    <ProductCard product={product} key={index} />
-                ))}
-            </div>
+            {!loading ? (
+                <div className="grid grid-cols-1 gap-5 md:grid-cols-4">
+                    {newProductsData.slice(0, 4).map((product, index) => (
+                        <ProductCard product={product} key={index} />
+                    ))}
+                </div>
+            ) : (
+                <Skeleton
+                    active
+                    paragraph={{ rows: 4 }}
+                    className="h-full w-full"
+                >
+                    <SectionCommon className="flex flex-col gap-10">
+                        <PageHader title="Sản phẩm nổi bật" />
+                        <div className="grid grid-cols-1 gap-5 md:grid-cols-4">
+                            {newProductsData
+                                .slice(0, 4)
+                                .map((product, index) => (
+                                    <ProductCard
+                                        product={product}
+                                        key={index}
+                                    />
+                                ))}
+                        </div>
+                    </SectionCommon>
+                </Skeleton>
+            )}
         </SectionCommon>
     )
 }
