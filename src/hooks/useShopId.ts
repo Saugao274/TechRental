61ef2d970444b97d3eda5c19d9cc079bef0e8b6f
Duc Anh <anhduong764027@gmail.com>
2025-05-29 04:52:19 +0700

    resolve conflict in useShopId

--- src/hooks/useShopId.ts
+++ src/hooks/useShopId.ts
@@ -1,43 +1,4 @@
-// hooks/useShopId.ts
-<<<<<<< HEAD
-"use client"
 
-import { useState, useEffect } from "react"
-import constants from "@/settings/constants"
-import webStorageClient from "@/utils/webStorageClient"
-
-export const useShopId = () => {
-  const [shopId, setShopId] = useState<string | null>(null)
-
-  useEffect(() => {
-    const fetchShopId = async () => {
-      try {
-        const token = webStorageClient.get(constants.ACCESS_TOKEN)
-
-        const res = await fetch(`${constants.API_SERVER}/api/shopDetail/me`, {
-          method: "GET",
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-          credentials: "include",
-        })
-
-        const data = await res.json()
-        if (res.ok) {
-          setShopId(data.metadata._id)
-        } else {
-          console.error("ShopDetail fetch failed:", data.message)
-        }
-      } catch (err) {
-        console.error("Lỗi khi lấy shopId:", err)
-      }
-    }
-
-    fetchShopId()
-  }, [])
-
-  return shopId
-=======
 'use client'
 
 import { useState, useEffect } from 'react'
@@ -78,5 +39,5 @@ export const useShopId = () => {
     }, [])
 
     return shopId
->>>>>>> 6ea8f1d20b40c0eb7b72622383fb1737245341c4
+
 }
